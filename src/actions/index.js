6f92b694e4dfc75e8ef74635a61e1d3ece1f05f0
EsraaAlaaOmar <esraa.safa55@gmail.com>
2021-11-19 23:38:31 +0200

    withoust wish and card

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,14 +3,11 @@ import { ADD_PRODUCT } from "./types"
 
 
 export const AddNewProduct = (pdoduct_info)  => {
-   
+    const {name,price,fImage,secImage,thirImage,describtion}=pdoduct_info
     console.log(pdoduct_info,'from actions')
     return{
         type:ADD_PRODUCT,
         payload: pdoduct_info
     }
- 
+    console.log(pdoduct_info,'from actions')
 }
-
-
-const {name,price,fImage,secImage,thirImage,describtion}=pdoduct_info
